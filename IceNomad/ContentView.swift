9326f0e0ed0244f4c3f03371830fce0b7a40dd09
whiteice217 <41704911+whiteice217@users.noreply.github.com>
2026-07-06 13:00:07 -0400

    Initial Commit

---
+++ IceNomad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IceNomad
+//
+//  Created by Bryan Stern on 7/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
